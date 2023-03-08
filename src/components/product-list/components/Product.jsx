6fc93d03e41425882311e5224d18491104047316
tepavcevic <djordje.tepa@gmail.com>
2023-03-08 16:00:28 +0100

    Refactoring CSS not to use z-index removing double borders

--- src/components/product-list/components/Product.jsx
+++ src/components/product-list/components/Product.jsx
@@ -4,8 +4,10 @@ export default function Product({ model, price, modelImage }) {
     return(
         <div className="product-card">
             <img className="product-image" src={modelImage} alt="sneaker model image" />
-            <p className="shoe-model-title">{model}</p>
-            <p className="shoe-model-price">${price}</p>
+            <div className="product-card-body">
+            <p className="product-model-title">{model}</p>
+            <p className="product-model-price">${price}</p>
+            </div>
         </div>
     )
 }
